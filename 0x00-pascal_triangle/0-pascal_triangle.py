bb61bcf4a2d9305dd43f067687e16a41c62146c9
godfreylehokoG <hlonilehoko29@yahoo.com>
2022-12-23 02:58:04 +0200

    Pushing Task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,20 +1,31 @@
 #!/usr/bin/python3
-"""
-Returns a Pascal's Triangle
-"""
+"""returns a pascal triangle"""
 
 
 def pascal_triangle(n):
-    """Returns a list of lists
-    of integers representing the Pascal’s triangle of n
     """
-    pascal = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            pascal.append(level)
+    returns a list of integers in a pascal triangle format
+    """
+
+    if n <= 0:
+        # return empty list
+        return []
+    pascal = [[1]]
+    if n == 1:
+        return pascal
+
+    for i in range(1, n):
+        left = -1
+        right = 0
+        in_pas = []
+        for j in range(i + 1):
+            num = 0 
+            if left > -1:
+                num += pascal[i - 1][left]
+            if right < i:
+                num += pascal[i - 1][right]
+            left += 1
+            right += 1
+            in_pas.append(num)
+        pascal.append(in_pas)
     return pascal
