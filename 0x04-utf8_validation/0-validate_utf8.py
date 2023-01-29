602bd4f618df03e60e3d20e599f2b92c4c462746
godfreylehokoG <hlonilehoko29@yahoo.com>
2023-01-30 00:42:40 +0200

    0-validate_utf8.py [14]

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,13 @@ Define validUTF8(data) function that validates whether a
 string of ints represents a valid UTF-8 encoding.
 """
 def validUTF8(data):
-        """ doc """
-        try:
-            maskeddata = [n & 255 for n in data]
-            bytes(maskeddata).decode("UTF-8")
-            return True
-        except Exception:
-            return False
+    """
+    Helper function
+    Convert ints to bits
+    """
+    try:
+        maskeddata = [n & 255 for n in data]
+        bytes(maskeddata).decode("UTF-8")
+        return True
+    except Exception:
+        return False
